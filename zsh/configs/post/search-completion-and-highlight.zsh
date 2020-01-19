fa4d288d40081c090caaa92213891b3a8ec9dfb6
Nicholas Cronquist <nick@ncronquist.com>
2020-01-18 22:01:56 -0500

    Adds Editorconfig File
    - Reindents other files to match editorconfig settings

--- zsh/configs/post/search-completion-and-highlight.zsh
+++ zsh/configs/post/search-completion-and-highlight.zsh
@@ -2,9 +2,9 @@
 autoload -U compinit
 compinit
 
-# According to https://github.com/zsh-users/zsh-syntax-highlighting, 
+# According to https://github.com/zsh-users/zsh-syntax-highlighting,
 # zsh-syntax-highlighting must be sourced after running compinit
-# In orer to use zsh-syntax-highlighting with zsh-history-substring search, 
+# In orer to use zsh-syntax-highlighting with zsh-history-substring search,
 # highlighting must be loaded first
 source "$HOME/.local/share/zsh/functions/zsh-syntax-highlighting"
 source "$HOME/.local/share/zsh/functions/zsh-history-substring-search"
@@ -14,9 +14,9 @@ source "$HOME/.local/share/zsh/functions/zsh-history-substring-search"
 # The binding has to happen after the zsh-history-substring-search
 # function has been sourced
 if [[ -z "$WSL_DISTRO_NAME" ]]; then
-    bindkey '^[[A' history-substring-search-up
-    bindkey '^[[B' history-substring-search-down 
+  bindkey '^[[A' history-substring-search-up
+  bindkey '^[[B' history-substring-search-down
 else
-    bindkey "$terminfo[kcuu1]" history-substring-search-up
-    bindkey "$terminfo[kcud1]" history-substring-search-down
+  bindkey "$terminfo[kcuu1]" history-substring-search-up
+  bindkey "$terminfo[kcud1]" history-substring-search-down
 fi
