61c27334ac47478a2b94d54c0307eb9fecd113e9
Nicholas Cronquist <nick@ncronquist.com>
2020-02-04 16:24:11 -0500

    Moves Zsh History File
    - oh-my-zsh defaults the zsh history file to ~/.zhistory
    - Switching to the oh-my-zsh standard even though I don't use
      oh-my-zsh since I use some tools created for oh-my-zsh

--- zsh/configs/history.zsh
+++ zsh/configs/history.zsh
@@ -1,5 +1,5 @@
 setopt hist_ignore_all_dups # Delete old recorded entry if new entry is a duplicate.
 setopt inc_append_history # Write to the history file immediately, not when the shell exits.
-HISTFILE=~/.zsh_history
+HISTFILE=~/.zhistory
 HISTSIZE=4096
 SAVEHIST=4096
