76c1bff6197100d6512f49118d5daf2d1bd6016c
Nicholas Cronquist <nick@ncronquist.com>
2020-02-05 16:23:06 -0500

    Bxaws tool was renamed bcscli

---
+++ zsh/configs/bcscli.zsh
@@ -0,0 +1,3 @@
+if [[ -f "$HOME/Documents/projects/bcscli/bcs" ]]; then
+    source $HOME/Documents/projects/bcscli/bcs
+fi
--- zsh/configs/bxaws.zsh
+++
@@ -1,3 +0,0 @@
-if [[ -f "$HOME/Documents/projects/bxaws/bxaws" ]]; then
-    source $HOME/Documents/projects/bxaws/bxaws
-fi
