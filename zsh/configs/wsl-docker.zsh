a8b5240b1af771c5f445d3ef3c39b0c117261ab0
Nicholas Cronquist <nick@ncronquist.com>
2020-08-29 16:29:55 -0400

    config: Don't Export Docker Host on WSL 2
    - When using WSL 2, either the WSL 2 Docker backend will be used or
      docker will be installed directly into the WSL 2 VM; Either way, the
      docker host variable does not need to be set

--- zsh/configs/wsl-docker.zsh
+++ zsh/configs/wsl-docker.zsh
@@ -1,4 +1,9 @@
-if grep -qE "(Microsoft|WSL)" /proc/version &>/dev/null; then
+# WSL 2 specific settings.
+if grep -q "microsoft" /proc/version &>/dev/null; then
+  # Don't set DOCKER_HOST for WSL 2
+  # Do nothing
+# WSL 1 specific settings.
+elif grep -qE "(Microsoft|WSL)" /proc/version &>/dev/null; then
   export DOCKER_HOST=tcp://localhost:2375
 fi
 
