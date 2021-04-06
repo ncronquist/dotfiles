736a69d66fc0838e6c5a637871148617b47ef530
Nicholas Cronquist <nick@ncronquist.com>
2021-04-06 17:09:55 -0400

    WIP: Mac Updates
    - Just a dump of the changes I've made since switching to work on a
      Macbook
    - Need to go through and clean this all up

--- zsh/configs/wsl-host.zsh
+++ zsh/configs/wsl-host.zsh
@@ -1,3 +1,5 @@
 # Reddit thread - t.ly/s6B9
 
-export wslip=$(ip addr sh $(ip route list default | grep -Po ' dev \K\w+') | grep -Po ' inet \K[\d.]+')
+if grep -qi "microsoft" /proc/version &>/dev/null; then
+    export wslip=$(ip addr sh $(ip route list default | grep -Po ' dev \K\w+') | grep -Po ' inet \K[\d.]+')
+fi
